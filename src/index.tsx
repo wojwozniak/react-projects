53505293bdbd19f81e9ccc07059d7fefc9d7bdc0
pulchnymamut <pulchnymamut@gmail.com>
2022-06-06 22:30:40 +0200

    Rebuilt

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Top from '../components/Top';
-import Editor from '../components/Editor';
-import Preview from '../components/Preview';
-import Foot from '../components/Foot';
+import Top from './components/Top';
+import Editor from './components/Editor';
+import Preview from './components/Preview';
+import Foot from './components/Foot';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
