7c481dbe7fe404276f03fffbc4fd34ffa9350e0e
pulchnymamut <pulchnymamut@gmail.com>
2022-06-07 23:22:04 +0200

    Added better styling

--- src/components/Foot.tsx
+++ src/components/Foot.tsx
@@ -5,6 +5,7 @@ const Foot: React.FunctionComponent = () => {
         <div id="foot">
             <p id="subtitle">Made by <a href="https://twitter.com/woj_wozniak" target="_blank">woj.wozniak</a> for freecodecamp.org course</p>
             <p id="more"><a href="https://wojwozniak.github.io/" target="_blank">More projects</a></p>
+            <p>Drum sounds from <a href="https://freecodecamp.org">freecodecamp.org</a></p>
         </div>
     )
 }
