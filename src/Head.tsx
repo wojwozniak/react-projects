8b984e1d38d50fc723afb9d7efb2e9069bd4a6bb
pulchnymamut <pulchnymamut@gmail.com>
2022-06-08 20:25:17 +0200

    Added header and footer of app

---
+++ src/Head.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const Head:React.FunctionComponent = () => {
+    return (
+        <h1 id="title">
+            Calculator
+        </h1>
+  );
+}
+
+export default Head;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Head from './Head';
+import Foot from './Foot';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Head />
     <App />
+    <Foot />
   </React.StrictMode>
 );
