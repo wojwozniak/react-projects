93ba3ed6b5da7afaa2e9c3e095e723f6f45f41ab
pulchnymamut <pulchnymamut@gmail.com>
2022-06-09 21:37:36 +0200

    No overflow for screen

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type ScreenProps = {
     display: string;
 }
 
 const Screen:React.FunctionComponent<ScreenProps> = ({display}) => {
+  
+  // State of screen size
+  const [cl, newCl] = useState("");
+
+  // Make screen larger if there is more than nine signs on the screen (it will be passed to screen as a property)
+  useEffect(() => {
+    if (display.length > 9) {
+      newCl("big-screen");
+    } else {
+      newCl("");
+    }
+  },[display])
+  
   return (
-        <div id="screen">
-          <p id="screen-display">{display}</p>
+        <div id="screen" className={cl}>
+          <p id="screen-display" className={cl}>{display}</p>
       </div>
   )
 }
