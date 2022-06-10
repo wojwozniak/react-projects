30ee94928d52ef09bdd3b3f1bf00d1361197813e
pulchnymamut <pulchnymamut@gmail.com>
2022-06-10 23:49:31 +0200

    Basic html and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import './App.css';
+import './css.css';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div id="wrapper">
+      <div id="clock" className="timer">25:00</div>
+      <p id="label" className="timer">Session</p>
+      <p><a id="start">Start</a></p>
+      <div id="picker-wrap">
+        <div id="session" className="picker">
+          <button id="session-plus" className="btn"><span>+</span></button>
+          <p id="session-timer" className="show">25</p>
+          <button id="session-minus" className="btn"><span>-</span></button>
+        </div>
+        <div id="break" className="picker">
+          <button id="break-plus" className="btn"><span>+</span></button>
+          <p id="break-timer" className="show">5</p>
+          <button id="break-minus" className="btn"><span>-</span></button>
+        </div>
+      </div>
     </div>
   );
 }
