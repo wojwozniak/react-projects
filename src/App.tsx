08eef55735b601de3ecdc788be69fefc245483b9
pulchnymamut <pulchnymamut@gmail.com>
2022-06-11 11:43:55 +0200

    Basic state added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,91 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 import './css.css';
 
 function App() {
+
+  type Actions = 
+    | "start" | "stop" | "session-increase" | "session-decrease" | "break-increase" | "break-decrease"
+  
+  type State = {
+    clock: string,
+    label: string,
+    startstop: string,
+    running: boolean,
+    sessiontimer: number,
+    session: number,
+    breaktimer: number,
+    break: number
+  }
+
+  const initialState:State = {
+    clock: "25:00",
+    label: "Session",
+    startstop: "Start",
+    running: false,
+    sessiontimer: 25,
+    session: 1500,
+    breaktimer: 5,
+    break: 300
+  };
+
+  const reducer = (state: State, action: Actions) => {
+    let newState = state;
+    switch (action) {
+      case "start":
+        newState.running = true;
+        break;
+      case "stop":
+        newState.running = false;
+        break;
+      default:
+        throw new Error();
+    }
+    return newState;
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleStartStop = () => {
+    if (!state.running) {
+      dispatch("start");
+    } else {
+      dispatch("stop");
+    }
+  }
+
+  const handleBtn = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e);
+  }
+
+ /* useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("aaa");
+    }, 1000);
+  }, [state]); */
+
   return (
     <div id="wrapper">
-      <div id="clock" className="timer">25:00</div>
-      <p id="label" className="timer">Session</p>
-      <p><a id="start">Start</a></p>
+      <div id="clock" className="timer">{ state.clock }</div>
+      <p id="label" className="timer">{state.label}</p>
       <div id="picker-wrap">
-        <div id="session" className="picker">
-          <button id="session-plus" className="btn"><span>+</span></button>
-          <p id="session-timer" className="show">25</p>
-          <button id="session-minus" className="btn"><span>-</span></button>
+        <div id="session-wrap">
+          <div id="session" className="picker">
+            <button id="session-plus" onClick={(e)=>handleBtn(e)}  className="btn"><span>+</span></button>
+            <p id="session-timer" className="show">{ state.sessiontimer }</p>
+            <button id="session-minus" onClick={(e)=>handleBtn(e)} className="btn"><span>-</span></button>
+          </div>
+          <p className="sublabel">Session</p>
         </div>
-        <div id="break" className="picker">
-          <button id="break-plus" className="btn"><span>+</span></button>
-          <p id="break-timer" className="show">5</p>
-          <button id="break-minus" className="btn"><span>-</span></button>
+        <div id="break-wrap">
+          <div id="break" className="picker">
+            <button id="break-plus" onClick={(e)=>handleBtn(e)} className="btn"><span>+</span></button>
+            <p id="break-timer" className="show">{ state.breaktimer }</p>
+            <button id="break-minus" className="btn" onClick={(e)=>handleBtn(e)}><span>-</span></button>
+          </div>
+          <p className="sublabel">Break</p>
         </div>
       </div>
+      <a id="start" onClick={handleStartStop}>{ state.startstop }</a>
     </div>
   );
 }
